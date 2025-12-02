7339aa1ba62f71445c8a8f5d686b39404a9effae
ariehaugen <arieehaugen@gmail.com>
2025-12-02 14:26:04 +0100

    day 2 part 2

--- src/days/day2.ts
+++ src/days/day2.ts
@@ -11,7 +11,43 @@ const part1 = (lines: string): any => {
 
 };
 
-const part2 = (lines: string): any => false;
+const part2 = (lines: string): any => {
+    // const test = `11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124`;
+    const productIds = lines.split(/,/).filter(Boolean);
+    const ranges = productIds.flatMap(x => defineRange(Number(x.split('-')[0]), Number(x.split('-')[1])))
+
+    return ranges.reduce((acc, curr) => isRep(curr.toString()) ? acc += curr : acc, 0)
+};
+
+const isRep = (s: string) => {
+    const n = s.length;
+    if (n < 2) return false;
+
+    const z = computeZArray(s);
+
+    for (let i = 1; i < n; i++) {
+        if (z[i] === n - i && n % i === 0) return true
+    }
+    return false;
+}
+
+const computeZArray = (s:string) => {
+    const n = s.length;
+    const z = new Array(n).fill(0);
+
+    let l = 0, r = 0;
+
+    for (let i = 1; i < n; i++) {
+        if (i <= r) z[i] = Math.min(r - i + 1, z[i - l]);
+        while (i + z[i] < n && s[z[i]] === s[i + z[i]]) z[i]++;
+        if (i + z[i] - 1 > r) {
+            l = i;
+            r = i + z[i] - 1
+        }
+    }
+
+    return z;
+}
 
 const defineRange = (start: number, end: number): number[] => {
     const range: number[] = [];
