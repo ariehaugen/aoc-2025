7930408ed2e5ef52bfe89d9049dd9efc7600adfb
ariehaugen <arieehaugen@gmail.com>
2025-12-03 13:29:49 +0100

    new attempt

--- src/days/day3.ts
+++ src/days/day3.ts
@@ -5,29 +5,12 @@ const part1 = (lines: string): any => {
 234234234234278
 818181911112111`
 
-    const banks = test.split(/\r?\n/).filter(Boolean).map(x => x.split('').map(Number));
+    const banks = lines.split(/\r?\n/).filter(Boolean).map(x => x.split('').map(Number));
     return banks.reduce((acc, curr) => {
-        // const pairs = new Set<number>([])
-        // console.log(...curr)
-        // console.log(Number(Math.max(...curr).toString() + Math.max(...curr.filter(x => x !== Math.max(...curr))).toString()))
+        const uC = [...new Set(curr)];
+        const one = Math.max(...uC.slice(0, uC.length - 1))
+        const two = Math.max(...uC.slice(uC.indexOf(Math.max(...uC.slice(0, uC.length - 1))) + 1))
 
-        // const combo: [number,number] = [0,0]
-        // curr.forEach(x => {
-        //     // console.log(combo)
-        //     if (x > combo[0]) combo[0] = x
-        //     if (x > combo[1] && x !== combo[0]) combo[1] = x
-        // })
-
-        // console.log(combo)
-        // const result = Number(combo.join(''));
-        // return acc += result;
-        const internal = [...new Set(curr)];
-        const one = Math.max(...internal),
-        two = Math.max(...internal.slice(internal.indexOf(Math.max(...internal) + 1)));
-        // console.log(internal.slice(internal.indexOf(one + 1)))
-        
-        // const res = Number([...new Set(curr.filter(v => [...curr].sort((a,b) => b-a).slice(0,2).includes(v)))].join(''))
-        // console.log(one,two)
         return acc += Number([one,two].join(''))
     }, 0)    
 };
