5bebe04e89e007aa7b3b5496e0e0fa0fb5b4456c
Arie Haugen <oyvindet@gmail.com>
2025-12-04 23:14:55 +0100

    day 4

--- src/days/day4.ts
+++ src/days/day4.ts
@@ -1,5 +1,4 @@
-const part1 = (lines: string): any => {
-    const test = `..@@.@@@@.
+const test = `..@@.@@@@.
 @@@.@.@.@@
 @@@@@.@.@@
 @.@@@@..@.
@@ -8,49 +7,69 @@ const part1 = (lines: string): any => {
 .@.@.@.@@@
 @.@@@.@@@@
 .@@@@@@@@.
-@.@.@@@.@.`
+@.@.@@@.@.`;
+const part1 = (lines: string): any => {
+  const rows = lines
+    .split(/\r?\n/)
+    .filter(Boolean)
+    .map((x) => x.trim().split("")) as string[][];
 
-    const rows = test.split(/\r?\n/).filter(Boolean).map(x => x.trim().split(''))
-    // console.log(rows);
-    let total = 0;
-    const demo = [...rows];
-    for (let i = 0; i < rows.length;i++) {
-        for (let j = 0; j < rows[0].length; j++) {
-            if (rows[j][i] === '@' && checkAdjacent(rows, [j,i])) {
-                console.log([j,i])
-                demo[j][i] = 'x';
-                total++;
-            };
-        }
+  let total = 0;
+
+  for (let i = 0; i < rows.length; i++) { // y
+    for (let j = 0; j < rows[i]!.length; j++) { // x
+      if (rows[i]?.[j] === "@" && checkAdjacent(rows, [j, i])) {
+        total++;
+      }
     }
-    console.log(demo.map(x => x.join('')).join('\n'))
-    return total;
-}
+  }
+  return total;
+};
 
 const checkAdjacent = (arr: string[][], [x, y]: [number, number]): boolean => {
-    const rollsCoords: number[][] = [];
-    if (y > 0) {
-        if (arr[y-1][x - 1] === '@') rollsCoords.push([y-1, x - 1])
-        if (arr[y-1][x] === '@') rollsCoords.push([y-1, x])
-        if (arr[y-1][x + 1] === '@') rollsCoords.push([y-1, x + 1])
-    }
-    if (x > 0) {
-        if (arr[y][x-1] === '@') rollsCoords.push([y,x-1]) 
-    }
-    if (x < arr[0].length - 1) {
-        if (arr[y][x+1] === '@') rollsCoords.push([y,x+1])
-    }
-    if (y < arr.length - 1) {
-        if (arr[y+1][x-1] === '@') rollsCoords.push([y+1,x-1])
-        if (arr[y+1][x] === '@') rollsCoords.push([y+1,x])
-        if (arr[y+1][x+1] === '@') rollsCoords.push([y+1,x+1])
-    }
-if (y === 0 && x === 7) console.log(rollsCoords)
-    return rollsCoords.filter(Boolean).length < 4
-}
+  const rollsCoords: number[][] = [];
+  if (y > 0) {
+    if (["@", "x"].includes(arr[y - 1]![x - 1]!)) rollsCoords.push([y - 1, x - 1]);
+    if (["@", "x"].includes(arr[y - 1]![x]!)) rollsCoords.push([y - 1, x]);
+    if (["@", "x"].includes(arr[y - 1]![x + 1]!)) rollsCoords.push([y - 1, x + 1]);
+  }
+  if (x > 0) {
+    if (["@", "x"].includes(arr[y]![x - 1]!)) rollsCoords.push([y, x - 1]);
+  }
+  if (x < arr[y].length - 1) {
+    if (["@", "x"].includes(arr[y]![x + 1]!)) rollsCoords.push([y, x + 1]);
+  }
+  if (y < arr.length - 1) {
+    if (["@", "x"].includes(arr[y + 1]![x - 1]!)) rollsCoords.push([y + 1, x - 1]);
+    if (["@", "x"].includes(arr[y + 1]![x]!)) rollsCoords.push([y + 1, x]);
+    if (["@", "x"].includes(arr[y + 1]![x + 1]!)) rollsCoords.push([y + 1, x + 1]);
+  }
+  return rollsCoords.filter(Boolean).length < 4;
+};
 
 const part2 = (lines: string): any => {
-    return false;
-}
+  let rows = lines
+    .split(/\r?\n/)
+    .filter(Boolean)
+    .map((x) => x.trim().split("")) ?? [[]];
+  let total = 0;
+  while (true) {
+    let current = 0;
+    for (let i = 0; i < rows.length; i++) { // y
+      for (let j = 0; j < rows[i]!.length; j++) { // x
+        if (rows[i]![j] === "@" && checkAdjacent(rows, [j, i])) {
+          rows[i]![j] = "x";
+          current++;
+        }
+      }
+    }
+    if (current === 0) break;
+    
+    rows = rows.map(row => row.map(col =>  col === 'x' ? '.' : col ))
+    
+    total += current;
+  }
+  return total;
+};
 
-export { part1, part2 }
+export { part1, part2 };
