2fd475be602dd3345143de5b3d2cc393f7f00632
ariehaugen <arieehaugen@gmail.com>
2025-12-08 11:48:37 +0100

    added counterclockwise rotating

--- src/days/day6.ts
+++ src/days/day6.ts
@@ -6,14 +6,7 @@ const test = `123 328  51 64
 
 const part1 = (lines: string): any => {
     const problems = lines.split(/\r?\n/).map(x => x.split(' ').filter(Boolean)).filter(x => x.length);
-    const [rows, cols] = [problems.length, problems[0]!.length];
-    const equations: string[][] = Array.from({ length: cols }, () => Array.from({ length: rows }));
-
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            equations[j][rows - 1 - i] = problems[i]![j]
-        }
-    }
+    const equations = rotate(problems)
     return equations.reduce((acc, curr) => {
         let sum = 0;
         let op = '+';
@@ -28,8 +21,24 @@ const part1 = (lines: string): any => {
     }, 0)
 }
 
+
 const part2 = (lines: string): any => {
-    return false;
+    const problems = test.split(/\r?\n/).map(x => x.split(' ').filter(Boolean)).filter(x => x.length);
+    
+    
+    const r = rotate(problems, true)
+    return '\n' + r.join('\n')
+}
+
+const rotate = (matrix: string[][], cc?:boolean): string[][] => {
+    const [rows, cols] = [matrix.length, matrix[0]?.length ?? 0];
+    const temp: string[][] = Array.from({ length: cols }, () => Array.from({length: rows }, () => ""));
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            temp[j]![(cc ? cols : rows) - 1 - i] = matrix[i]![j]!
+        }
+    }
+    return temp;
 }
 
 export { part1, part2 }
